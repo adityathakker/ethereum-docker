2f4f249bc62a1cf8447f655ca82c4c1b77732ee7
Aditya Thakker <aditya.j.thakker@gmail.com>
2022-11-05 16:39:49 -0700

    Only dev chain now

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
-cd /root/eth-net-intelligence-api
-perl -pi -e "s/XXX/$(hostname)/g" app.json
-/usr/local/bin/pm2 start ./app.json
-sleep 3
 geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
-sleep 3
-BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
-GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
-geth $GETH_OPTS
+sleep 1
+geth --dev --dev.period 13 --http --http.api eth,web3,personal,net --http.corsdomain "*"
