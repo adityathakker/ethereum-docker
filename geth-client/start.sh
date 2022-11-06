a621b53363b7112d1a0768639416255e474ec088
Aditya Thakker <aditya.j.thakker@gmail.com>
2022-11-05 19:59:34 -0700

    make everything lean

--- geth-client/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
-sleep 1
-geth --dev --dev.period 13 --http --http.api eth,web3,personal,net --http.corsdomain "*"
